086b5f9df7c32983b93e872b34e70a5fa55fe2fa
kininormal <kini.unik@gmail.com>
2026-08-28 12:03:48 +0200

    Basis django setup urls for app added one view added for index file

--- Lingvistik_proj/Lingvistik_app/views.py
+++ Lingvistik_proj/Lingvistik_app/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse, redirect
 # Create your views here.
+def index(request):
+    return render(request, 'Lingvistik_app/index.html')
