98738ffff25b29df6639ea714ae99cb8cc1ce9d3
kininormal <kini.unik@gmail.com>
2026-08-28 15:49:47 +0200

    Fist version of stats fledgling beginning of data build

--- Lingvistik_proj/Lingvistik_app/management/commands/stats.py
+++ Lingvistik_proj/Lingvistik_app/management/commands/stats.py
@@ -1,34 +1,83 @@
+#about stats.py:  https://www.geeksforgeeks.org/python/custom-django-management-commands/python manage.py stats
 from django.core.management.base import BaseCommand
 import matplotlib.pyplot as plt
 import nltk
-from huggingface_hub import logipythonn
-#import datasets
+import pandas as pd
+from huggingface_hub import login
+import datasets
 from datasets import load_dataset
 class Command(BaseCommand):
     help = 'Update data basis'
 
     def handle(self, *args, **kwargs):
       # You can specify the quality parameter here or get it from args/kwargs
-       
-      update_english_data()
-      #update_danish_data() #comming update_danish_data()
+      #create empty language_df
+      language_df = pd.DataFrame()
+      #add english to language_df
+      language_df  = update_english_data(language_df, 'english')
+      #add danish 
+      update_danish_data(language_df, 'danish') 
          
-      self.stdout.write(self.style.SUCCESS('Successfully updated diamond and iris plots in the database.'))
-def  update_english_data():
+      self.stdout.write(self.style.SUCCESS('Successfully updated language data - just initial start.'))
+def  update_english_data(df, lang):
+   
+   #Build basis for english language data
    
    #English sets version one
+   source = 'gutenberg'
+   name_of_work = 'austen-emma.txt'
    nltk.corpus.gutenberg.fileids()
    #Test working before building data and functions
-   emma = nltk.corpus.gutenberg.words('austen-emma.txt')
+   emma = nltk.corpus.gutenberg.words(name_of_work)
    print(len(emma))
    print(type(emma))
 
+   body_part = 'head'
 
+    # Create NLTK Text object
    emma_txt = nltk.Text(emma)
-   emma_txt.concordance('head') 
-   
-   print("English language data updated!- no implementation yet testing it seems to work") 
-def update_danish_data():
+
+   # Find all concordances
+   concordances = emma_txt.concordance_list(body_part)
+
+   # Build rows for dataframe
+   data = []
+
+   for concordance in concordances:
+         text = ' '.join(concordance.left) + ' ' \
+               + concordance.query + ' ' \
+               + ' '.join(concordance.right)
+         data.append([
+            text,
+            body_part,
+            lang,
+            source,
+            name_of_work
+         ])
+
+   # Create dataframe
+   new_data = pd.DataFrame(
+      data,
+         columns=[
+            'Text',
+            'Body part',
+            'Language',
+            'Source',
+            'Name_of_work'
+         ]
+   )
+
+   # Add new observations to existing dataframe
+   df = pd.concat([df, new_data], ignore_index=True)
+
+   print('Head of df with attributes:')
+   print(df.head())
+
+   print("English language data updated!")
+
+   return df
+def update_danish_data(df, lang):
+   #Build basis for danish language data
    # Implement the logic to update Danish language data here
    #Danish sets
    sampleLst = []
@@ -40,12 +89,12 @@ def update_danish_data():
    
    #Take first 10
    sampleLst = list(danish_set.take(10))  # 10 første elementer
-
+   body_part ='head'
    for memberNr, sampleMember in enumerate(sampleLst):
-      words = nltk.word_tokenize(sampleMember["text"], language="danish")
+      words = nltk.word_tokenize(sampleMember["text"], language=lang)
       text_obj = nltk.Text(words)
       print(f"--- Sample {memberNr} ---")
-      text_obj.concordance("hoved")
+      text_obj.concordance(body_part)
 
 
-   print("Danish language data updated! - no implementation yet  testing it seems to work")
+   print("Danish language data updated! - no implementation yet  testing it seems to work danis not handled")
