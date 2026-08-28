96ad7823acd2a21a358f61c6d0151d5b3baff6e0
kininormal <kini.unik@gmail.com>
2026-08-28 11:07:24 +0200

    Empty app added settings file add hosts and app

--- Lingvistik_proj/Lingvistik_proj/settings.py
+++ Lingvistik_proj/Lingvistik_proj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-g)mufu*rp$rnbixv1#+xhgdb9nhkayo$3d)xanb7jyd41ygf)&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Lingvistik_app',
 ]
 
 MIDDLEWARE = [
