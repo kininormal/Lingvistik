68c82907dbaa91f2af39dbe784b91838269caac1
kininormal <kini.unik@gmail.com>
2026-08-31 14:09:53 +0200

    more tham 18 wors handling of tCorpusReader secyrity missing

--- Lingvistik_proj/Lingvistik_app/management/commands/stats.py
+++ Lingvistik_proj/Lingvistik_app/management/commands/stats.py
@@ -3,10 +3,13 @@ from django.core.management.base import BaseCommand
 import matplotlib.pyplot as plt
 import nltk
 import spacy
+from pathlib import Path
 import pandas as pd
 from huggingface_hub import login
 import datasets
 from datasets import load_dataset
+from nltk.corpus import gutenberg
+from nltk.corpus.reader import PlaintextCorpusReader
 
 
 class Command(BaseCommand):
@@ -16,20 +19,56 @@ class Command(BaseCommand):
    def handle(self, *args, **kwargs):
       # Load English spaCy model
       nlp_en = spacy.load("en_core_web_sm")
+      nlp_da = spacy.load("da_core_news_sm")
       # You can specify the quality parameter here or get it from args/kwargs
       #create empty language_df
       language_df = pd.DataFrame()
-      #add english text from corpus to language_df
-      language_df  = update_english_data(language_df, 'english',  nlp_en)
-      #add danish text from corpus to language_df
-      #update_danish_data(language_df, 'danish') 
+      #list_of_names  = [ ['head', 'body part'], ['foot', 'body part' ], ['heart', 'organ']] #tested works
+      list_of_names  = [ ['head', 'body part']]
+      for part in list_of_names:
+         body_name = part[0]
+         body_category = part[1]
+      
+         print(f"Searce tex for:  {body_name} with category {body_category}")
+         #add english text from corpus to language_df
+         language_df  = update_english_data(language_df, body_name, body_category, 'english',  nlp_en)
+         #add danish text from corpus to language_df
+         #update_danish_data(language_df, body_name, body_category, 'danish') 
+         
+         
+      print('Language df after updates - more languages full update ONLY ENGLISH SO FAR:')
+      rows, columns = language_df.shape
+      print(f"Rows in main function:  {rows}")
+      print("First rows")
+      print( language_df.head())
+      print("Last rows")
+      print( language_df.tail())
          
       self.stdout.write(self.style.SUCCESS('Successfully updated language data - just initial start.'))
-def  update_english_data(df, lang, nlp_lang):
+def  update_english_data(df, body_name, body_category, lang, nlp_lang):
+   #BASE_DIR = Path(__file__).resolve().parent
+   
+   # nltk known (allowd) paths
+   for path in nltk.data.path:
+      p = Path(path)
+      print('allowed knowen paht PlaintextCorpusReader promlem ') 
+      print(p, "→", p.exists())
+      print('nlkt paths')
+      print(nltk.data.path)
+   
+   BASE_DIR = Path(__file__).resolve().parents[3]
    source = 'gutenberg'
+   GUTENBERG_PATH = BASE_DIR /'data'/'gutenberg'
+   my_gutenberg = PlaintextCorpusReader(
+      str(GUTENBERG_PATH),
+      r".*\.txt"
+   )
+   print("MY works from gutenberg")
+   print(my_gutenberg.fileids())
    #work in gutenberg in fileids
    file_ids = nltk.corpus.gutenberg.fileids()
-   
+   print('number of works in gutenberg', len(file_ids))
+
    for fileindex in range(0, 2):
       name_of_work = file_ids[fileindex]
       print(f"Valgt værk: {name_of_work}")
@@ -37,91 +76,19 @@ def  update_english_data(df, lang, nlp_lang):
       # Build basis for English language data
       # ----------------------------------
 
-      body_name = 'head'
-      body_category = 'body part'
       df = handle_current_vork(df,source, name_of_work, body_name, body_category, lang, nlp_lang)
-   # # ----------------------------------
-   # # Load raw text from NLTK Gutenberg
-   # # ----------------------------------
-
-   # raw_text = nltk.corpus.gutenberg.raw(name_of_work)
-
-   # # ----------------------------------
-   # # Process text with spaCy
-   # # ----------------------------------
-
-   # doc = nlp_lang(raw_text)
-
-   # # ----------------------------------
-   # # Build rows
-   # # ----------------------------------
-   # data = []
-
-   # # Loop through sentences
-   # for sent in doc.sents:
-   #    # Loop through tokens in sentence
-   #    for token in sent:
-   #       # Find lemma "head"
-   #       if token.lemma_.lower() == body_name:
-   #          data.append([
-   #             sent.text,
-   #             token.text,
-   #             token.lemma_,
-   #             body_name,
-   #             body_category,
-   #             lang,
-   #             source,
-   #             name_of_work
-   #          ])
    
-   # # ----------------------------------
-   # # Create dataframe
-   # # ----------------------------------
-   # new_data = pd.DataFrame(
-   #    data,
-   #       columns=[
-   #          'Text',
-   #          'Found word',
-   #          'Lemma',
-   #          'Body name',
-   #          'Body category',
-   #          'Language',
-   #          'Source',
-   #          'Name of work'
-   #       ]
-   # )
-   
-   # # ----------------------------------
-   # # Append to existing dataframe
-   # # ----------------------------------
-   # df = pd.concat(
-   #    [df, new_data],
-   #    ignore_index=True
-   # )
-
-   # # ----------------------------------
-   # # Export
-   # # ----------------------------------
-
-   # df.to_excel(
-   #    'lingvistik.xlsx',
-   #    index=False
-   # )
    rows, columns = df.shape
-   print('HANDLED rows in df:')
-   print(rows)
-   print('Head of handled:')
-   print(df.head())
-
+   print(f"Rows in function for english:  {rows}")
    print("English language data updated!")
    return df
-def update_danish_data(df, lang):
+def update_danish_data(df,  body_name, body_category, lang):
    #Build basis for danish language data
    # Implement the logic to update Danish language data here
    #Danish sets
    sampleLst = []
    name = "danish-foundation-models/danish-gigaword"
-   danish_set= load_dataset(name, split = "train")
+   danish_set = load_dataset(name, split = "train")
    sample = danish_set[1] # see "Data Instances" below
    print('danish set index 1 keys', sample.keys())
    print('danish text index 1', sample["text"][:500])
@@ -130,12 +97,11 @@ def update_danish_data(df, lang):
    #print('danish text index 2', next_sample["text"][:500])
    #Take first 10
    sampleLst = list(danish_set.take(100))  # 100 første elementer
-   body_part ='head'
    for memberNr, sampleMember in enumerate(sampleLst):
       words = nltk.word_tokenize(sampleMember["text"], language=lang)
       text_obj = nltk.Text(words)
       print(f"--- Sample {memberNr} ---")
-      text_obj.concordance(body_part)
+      text_obj.concordance(body_name)
 
 
    print("Danish language data updated! - no implementation yet  testing it seems to work danis not handled")
@@ -211,7 +177,7 @@ def handle_current_vork(df, source, name_of_work, body_name, body_category, lang
          index=False
       )
    
-      print('Head of df with attributes:')
-      print(df.head(80))
+      print('Head of df with attributes in function:')
+      print(df.head())
       return df
    
