01dacbdac043bc679dbf4167f7de1ff753433c2d
kininormal <kini.unik@gmail.com>
2026-09-04 11:46:33 +0200

    efter git push

--- Lingvistik_proj/Lingvistik_app/management/commands/stats.py
+++ Lingvistik_proj/Lingvistik_app/management/commands/stats.py
@@ -31,7 +31,6 @@ class Command(BaseCommand):
          body_name = part[0]
          body_category = part[1]
       
-         print(f"Searce tex for:  {body_name} with category {body_category}")
          #add english text from corpus to language_df
          language_df  = update_english_data(language_df, body_name, body_category, 'english',  nlp_en)
          #add danish text from corpus to language_df
@@ -50,58 +49,20 @@ class Command(BaseCommand):
 def  update_english_data(df, body_name, body_category, lang, nlp_lang):
   
    BASE_DIR = Path(__file__).resolve().parents[4] #go to project root to access allowed Lingvistik_env/nltk_data/corpora/gutenberg for PlaintextCorpusReader
-   print(BASE_DIR)
-   source = 'gutenberg'
-   NLTK_PATH_PART= 'Lingvistik_env/nltk_data/corpora/'
-   NLTK_PATH_PART =  NLTK_PATH_PART + source
-   GUTENBERG_PATH = BASE_DIR / NLTK_PATH_PART #allowed path for PlaintextCorpusReader
    
-   file_ids = nltk.corpus.gutenberg.fileids()
-   print('number of works in gutenberg', len(file_ids))
-   nltk_works = [
-     (gutenberg, fileid)
-     for fileid in gutenberg.fileids()
-   ]
+   map_name_for_sources = 'corpora/gutenberg/'
+   #Allowe the path for PlaintextCorpusReader
+   MY_CORPUS_PATH = BASE_DIR / map_name_for_sources
+   nltk.data.path.append(str(MY_CORPUS_PATH)) #DO NOT USE allowed NLTK_PATH in virtual environment - Works from NLTK are then "lost" and not found by PlaintextCorpusReader - only works for nltk.corpus.gutenberg
    
-
+   gutenberg = nltk.corpus.gutenberg   
+   
    my_gutenberg = PlaintextCorpusReader(
-      str(GUTENBERG_PATH),
+      str((MY_CORPUS_PATH)),
       r".*\.txt",
       encoding="utf-8"
    )
    
-   #WORKING TESTS - to identify problems with the text files in the my_gutenberg corpus
-   # #Test 2 two identify charater problems in the text - it seems to be a problem with the encoding of the text file. The text file is encoded in UTF-8, but it contains some characters that are not valid UTF-8 characters. This can happen if the text file was created on a different platform or with a different encoding. To fix this, you can try to read the file with a different encoding, such as 'latin-1' or 'cp1252', which are more permissive and can handle a wider range of characters. You can also try to clean the text by removing or replacing invalid characters before processing it.
-   # file_path = GUTENBERG_PATH / "Mansfield Park.txt"
-
-   # # Test 2.1 – almindelig Python
-   # with open(file_path, "r", encoding="utf-8") as file:
-   #        text_python = file.read()
-
-   # print("PYTHON:")
-   # print(text_python[2500:5000])
-   
-   # cleaned_text_python = clean_text(text_python)
-   # print("PYTHON cleaned:")
-   # print(cleaned_text_python[2500:5000])
-
-
-   # # Test 2.2 – NLTK CorpusReader
-   # text_nltk = my_gutenberg.raw("Mansfield Park.txt")
-
-   # cleaned_text_nltk = clean_text(text_nltk)
-
-   # print("\nNLTK:")
-   # print(cleaned_text_nltk[2500:5000])
-   # print("PYTHON with repr - gives hitten:")
-   # print(repr(cleaned_text_python[2500:5000]))
-   # print("\nNLTK with repr - gives hitten:")
-   # print(repr(cleaned_text_nltk[2500:5000]))
-   
-   # my_works = [
-   #  (my_gutenberg, fileid)
-   #  for fileid in my_gutenberg.fileids()
-   # ]
    
    #Metadata for initial works nltk_works
    works = [
@@ -113,7 +74,7 @@ def  update_english_data(df, body_name, body_category, lang, nlp_lang):
     }
     for fileid in gutenberg.fileids()
    ]
-   
+ 
    #add works from my_gutenberg
    works += [
     {
@@ -123,15 +84,10 @@ def  update_english_data(df, body_name, body_category, lang, nlp_lang):
         "language": "English",
     }
     for fileid in my_gutenberg.fileids()
-]
-   #print("MY works from gutenberg")
-   #print(my_gutenberg.fileids())
-   #work in gutenberg in fileids
-   #all_works = nltk_works + my_works
-   #for corpus, fileid in all_works:
-    #  print(fileid)
+   ]
+  
    #loop though works and process each work   
-   for work in works[:1]: #testing with first 1 works
+   for work in works[:2]: #testing with first 2 works
       corpus = work["corpus"]
       fileid = work["fileid"]
 
@@ -139,9 +95,7 @@ def  update_english_data(df, body_name, body_category, lang, nlp_lang):
 
       source = work["source"]
       language = work["language"]
-      #check if loop through works is correct before processing
-      print(f"Processing work: {fileid} from source: {source} in language: {language}")
-      print(f"Valgt værk: {fileid}")
+      
       # ----------------------------------
       # Build basis for English language data for current work
       # ----------------------------------
@@ -177,88 +131,89 @@ def update_danish_data(df,  body_name, body_category, lang):
    print("Danish language data updated! - no implementation yet  testing it seems to work danis not handled")
 
 
-#only works for my_gutenberg corpus - not for nltk gutenberg corpus
 def handle_current_english_work(df, my_gutenberg, source, name_of_work, body_name, body_category, lang, nlp_lang):
-    # ----------------------------------
-      # Load raw text from NLTK Gutenberg
-      # ----------------------------------
-   
-      raw_text =  my_gutenberg.raw(name_of_work)
+  
+   # Check source Then handle  Clean text according to Gutenberg corpus (NLKT often adds \r to \n, and my_gutenberg may have different formatting)  
+   if source == "NLTK Gutenberg":   
+           
+      raw_text = nltk.corpus.gutenberg.raw(name_of_work)          
       text = clean_text(raw_text)
-      #Test if text is cleaned correctly
-      print(f"Text from {name_of_work} after cleaning IN HANDLER:")
-      print(text[2500:5000])
-      
-      
-     
+   elif source == "Project Gutenberg":          
+       
+      raw_text =  my_gutenberg.raw(name_of_work)          
+      text = clean_text(raw_text)
+   else:          
+      print(f"PROBLEM: Unknown source: {source}. Skipping work: {name_of_work}")        
+      return df  # Skip processing for unknown sources
+   
+   # ----------------------------------
+   # Clean text according to Gutenberg corpus
+   # ----------------------------------
+   
+   # ----------------------------------
+   # Process text with spaCy
+   # ----------------------------------
 
+   doc = nlp_lang(text)
+
+   # ----------------------------------
+   # Build rows
+   # ----------------------------------
+   data = []
+
+   # Loop through sentences
+   for sent in doc.sents:
+      # Loop through tokens in sentence
+      for token in sent:
+         # Find lemma "head"
+         if token.lemma_.lower() == body_name:
+            data.append([
+               sent.text,
+               token.text,
+               token.lemma_,
+               body_name,
+               body_category,
+               lang,
+               source,
+               name_of_work
+            ])
    
-      
-      
-      # ----------------------------------
-      # Process text with spaCy
-      # ----------------------------------
+   # ----------------------------------
+   # Create dataframe
+   # ----------------------------------
+   new_data = pd.DataFrame(
+      data,
+         columns=[
+            'Text',
+            'Found word',
+            'Lemma',
+            'Body name',
+            'Body category',
+            'Language',
+            'Source',
+            'Name of work'
+         ]
+   )
    
-      doc = nlp_lang(text)
-   
-      # ----------------------------------
-      # Build rows
-      # ----------------------------------
-      data = []
-   
-      # Loop through sentences
-      for sent in doc.sents:
-         # Loop through tokens in sentence
-         for token in sent:
-            # Find lemma "head"
-            if token.lemma_.lower() == body_name:
-               data.append([
-                  sent.text,
-                  token.text,
-                  token.lemma_,
-                  body_name,
-                  body_category,
-                  lang,
-                  source,
-                  name_of_work
-               ])
-      
-      # ----------------------------------
-      # Create dataframe
-      # ----------------------------------
-      new_data = pd.DataFrame(
-         data,
-            columns=[
-               'Text',
-               'Found word',
-               'Lemma',
-               'Body name',
-               'Body category',
-               'Language',
-               'Source',
-               'Name of work'
-            ]
-      )
-      
-      # ----------------------------------
-      # Append to existing dataframe
-      # ----------------------------------
-      df = pd.concat(
-         [df, new_data],
-         ignore_index=True
-      )
-   
-      # ----------------------------------
-      # Export
-      # ----------------------------------
-      df.to_excel(
-         'lingvistik.xlsx',
-         index=False
-      )
-   
-      print('Head of df with attributes in function:')
-      print(df.head())
-      return df
+   # ----------------------------------
+   # Append to existing dataframe
+   # ----------------------------------
+   df = pd.concat(
+      [df, new_data],
+      ignore_index=True
+   )
+
+   # ----------------------------------
+   # Export
+   # ----------------------------------
+   df.to_excel(
+      'lingvistik.xlsx',
+      index=False
+   )
+
+   print('Head of df with attributes in function:')
+   print(df.head())
+   return df
 def normalize_text(text):
     """Gør forskellige linjeskift ens."""
 
@@ -276,7 +231,9 @@ def clean_text(text):
    # except (UnicodeEncodeError, UnicodeDecodeError):
    #    pass
    text = normalize_text(text)
-    # Alle former for linjeskift erstattes med mellemrum
+   text = text.replace("_", " ") # eg jame ausin uses _ insted of blank to indicate words said with litte pause between them in some works
+   text = text.replace("--", "  ") # eg jame ausin uses --  to indicate renovation pause
+   # Alle former for linjeskift erstattes med mellemrum
    text = text.replace("\n", " ")
 
     # Flere whitespace-tegn reduceres til ét mellemrum
