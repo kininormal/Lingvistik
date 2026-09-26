ccaaa0f5b73a37f7ada02476acfdad131e0bc068
kininormal <kini.unik@gmail.com>
2026-09-26 12:06:09 +0200

    Basale rettelser og forberedelse til nedarvning

--- Lingvistik_proj/Lingvistik_app/management/commands/statsservices/meaning.py
+++ Lingvistik_proj/Lingvistik_app/management/commands/statsservices/meaning.py
@@ -23,7 +23,11 @@ class PreviousSentence:
       self.sentence = previoussentence
       self.lemma_sentence_ref = lemma_sentence_obj  # Reference til LemmaSentence-objektet
       self.is_lemma_present = None
-
+   def set_lemma_present(self, is_lemma_present):
+     self.is_lemma_present = is_lemma_present
+   def has_lemma(self):
+      #if return value is None: Has not been set
+      return self.is_lemma_present
    def get_parent_sentence(self):
       return self.lemma_sentence_ref.get_sentence()
    def get_sentenceID(self):
@@ -31,15 +35,56 @@ class PreviousSentence:
      
 class FollowingSentence:
    def __init__(self, followingsentence, lemma_sentence_obj):
-      self.name = 'Previous sentence'
+      self.name = 'Following sentence'
       self.sentence = followingsentence
       self.lemma_sentence_ref = lemma_sentence_obj  # Reference til LemmaSentence-objektet
       self.is_lemma_present = None
+      
+   def set_lemma_present(self, is_lemma_present):
+      self.is_lemma_present = is_lemma_present
+   def has_lemma(self):
+      #if return value is None: Has not been set
+      return self.is_lemma_present
 
    def get_parent_sentence(self):
       return self.lemma_sentence_ref.get_sentence()
    def get_sentenceID(self):
       return self.lemma_sentence_ref.get_sentenceID()
+
+#Version 2 noter 
+# class BaseSentence:
+#     """En fælles basisklasse til at håndtere delt logik for sætninger."""
+#     def __init__(self, name, sentence, lemma_sentence_obj):
+#         self.name = name
+#         self.sentence = sentence
+#         self.lemma_sentence_ref = lemma_sentence_obj  # Reference til LemmaSentence-objektet
+#         self.is_lemma_present = None
+
+#     def set_lemma_present(self, is_lemma_present):
+#         self.is_lemma_present = is_lemma_present
+
+#     def has_lemma(self):
+#         # Hvis returværdien er None: Er ikke blevet sat endnu
+#         return self.is_lemma_present
+
+#     def get_parent_sentence(self):
+#         return self.lemma_sentence_ref.get_sentence()
+
+#     def get_sentenceID(self):
+#         return self.lemma_sentence_ref.get_sentenceID()
+
+
+# class PreviousSentence(BaseSentence):
+#     def __init__(self, previoussentence, lemma_sentence_obj):
+#         # super() kalder __init__ i basisklassen og sender de korrekte værdier med
+#         super().__init__('Previous sentence', previoussentence, lemma_sentence_obj)
+
+
+# class FollowingSentence(BaseSentence):
+#     def __init__(self, followingsentence, lemma_sentence_obj):
+#         # super() kalder __init__ i basisklassen og sender de korrekte værdier med
+#         super().__init__('Following sentence', followingsentence, lemma_sentence_obj)
+
 
 
 
